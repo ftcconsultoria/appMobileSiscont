cc5789b049569600f87d5d2e77258862659d46d9
ftcconsultoria <contato@ftcc.com.br>
2025-06-12 15:54:10 -0300

    Update DPR file to new main form

--- appMobileSiscont.dpr
+++ appMobileSiscont.dpr
@@ -3,27 +3,18 @@ program appMobileSiscont;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  MainForm in 'src\MainForm.pas' {frmMain},
-  ProductsForm in 'src\ProductsForm.pas' {frmProducts},
-  ProductEditForm in 'src\ProductEditForm.pas' {frmProductEdit},
-  ClientsForm in 'src\ClientsForm.pas' {frmClients},
+  Unit1 in 'Unit1.pas' {Form1},
   ClientEditForm in 'src\ClientEditForm.pas' {frmClientEdit},
-  DataModule in 'src\DataModule.pas' {dmData: TdmData},
-  LoginForm in 'src\LoginForm.pas' {frmLogin},
-  SplashForm in 'src\SplashForm.pas' {frmSplash};
+  ClientsForm in 'src\ClientsForm.pas' {frmClients},
+  DataModule in 'src\DataModule.pas' {dmData: TDataModule},
+  MainForm in 'src\MainForm.pas' {frmMain},
+  ProductEditForm in 'src\ProductEditForm.pas' {frmProductEdit},
+  ProductsForm in 'src\ProductsForm.pas' {frmProducts};
 
 {$R *.res}
 
 begin
   Application.Initialize;
-  Application.CreateForm(TdmData, dmData);
   Application.CreateForm(TfrmMain, frmMain);
-  Application.CreateForm(TfrmLogin, frmLogin);
-  Application.CreateForm(TfrmSplash, frmSplash);
-  Application.CreateForm(TfrmProducts, frmProducts);
-  Application.CreateForm(TfrmProductEdit, frmProductEdit);
-  Application.CreateForm(TfrmClients, frmClients);
-  Application.CreateForm(TfrmClientEdit, frmClientEdit);
   Application.Run;
 end.
-
