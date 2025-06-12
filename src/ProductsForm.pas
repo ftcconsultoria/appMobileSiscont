29bd06257aeb50c24df235b8cd28a825c78df5dd
ftcconsultoria <contato@ftcc.com.br>
2025-06-12 16:14:00 -0300

    Add FMX.SearchBox uses

--- src/ProductsForm.pas
+++ src/ProductsForm.pas
@@ -6,7 +6,7 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   FMX.Controls.Presentation, FMX.Layouts, FMX.ListView.Types, FMX.ListView,
-  FMX.Edit, Theme;
+  FMX.Edit, FMX.SearchBox, Theme;
 
 type
   TfrmProducts = class(TForm)
